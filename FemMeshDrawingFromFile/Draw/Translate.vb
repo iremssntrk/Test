9ff85b2e4319a9cbc52f4589e9f931599e1e245c
isenturk <iremssntrk@gmail.com>
2023-03-29 09:42:25 +0300

    FemMeshDrawingFromFile projesi backup1

---
+++ FemMeshDrawingFromFile/Draw/Translate.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Runtime.Remoting
+Imports devDept.Eyeshot
+Imports devDept.Eyeshot.Entities
+Imports devDept.Eyeshot.Fem
+Imports devDept.Eyeshot.FontStyleCharData
+Imports devDept.Geometry
+Imports devDept.Geometry.Entities
+Imports devDept.Graphics
+
+Public Class Translate
+
+    Dim _model As Simulation
+    Public Sub New(Model As Simulation)
+        _model = Model
+    End Sub
+
+    Sub SumVertices(vertices As List(Of Point3D), item As String)
+        Dim tArray() As String
+        tArray = Split(item, " ")
+        Dim p(3) As Double
+        For i = 0 To UBound(tArray)
+            Double.TryParse(tArray(i), p(i))
+        Next i
+        Dim v As Node = New Node(p(0), p(1), p(2))
+        vertices.Add(v)
+    End Sub
+
+    Sub SumElement(Mat As Material, vertices As List(Of Point3D), item As String)
+        Dim element As List(Of Integer) = New List(Of Integer)
+
+        Dim temp As Integer
+        Dim tArray() As String
+        tArray = Split(item, " ")
+        For k = 0 To UBound(tArray)
+            Double.TryParse(tArray(k), temp)
+            element.Add(temp)
+        Next k
+        If (element.Count) = 4 Then
+            FemmeshGenerator(element, vertices, Mat)
+        ElseIf (element.Count) = 3 Then
+            MeshGenerator(element, vertices, Mat)
+        End If
+
+    End Sub
+
+
+    Sub FemmeshGenerator(element As List(Of Integer), vertices As List(Of Point3D), mat As Material)
+        Dim triangles As List(Of Element) = New List(Of Element)
+        Dim quad = New Quad4(element(0), element(1), element(2), element(3), mat)
+        triangles.Add(quad)
+        Dim mesh As FemMesh = New FemMesh(vertices, triangles)
+        mesh.ColorMethod = colorMethodType.byEntity
+        _model.Entities.Add(mesh)
+    End Sub
+
+    Sub MeshGenerator(element As List(Of Integer), vertices As List(Of Point3D), mat As Material)
+        'Dim triangles As List(Of IndexTriangle) = New List(Of IndexTriangle)
+        Dim triangles As List(Of Element) = New List(Of Element)
+        Dim color As Color = New Color()
+        color.R = 5
+        color.G = 5
+        color.B = 10
+        Dim tri1 As Tria3 = New Tria3(element(0), element(1), element(2), mat)
+        triangles.Add(tri1)
+        Dim mesh As FemMesh = New FemMesh(vertices, triangles)
+        mesh.ColorMethod = colorMethodType.byEntity
+        _model.Entities.Add(mesh)
+    End Sub
+
+End Class
+
+
---
+++ FemMeshDrawingFromFile/MainWindow.xaml.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Runtime.Remoting
+Imports devDept.Eyeshot.Entities
+Imports devDept.Eyeshot.Fem
+Imports devDept.Geometry
+Imports devDept.Geometry.Entities
+Imports devDept.Graphics
+
+Class MainWindow
+    Dim streamClass As StreamRW
+    Dim translate As Translate
+    Dim quad As Quad4
+    Public Sub New()
+        InitializeComponent()
+        streamClass = New StreamRW()
+        translate = New Translate(sim1)
+        sim1.ActionMode = devDept.Eyeshot.actionType.SelectVisibleByPick
+
+    End Sub
+
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+        Dim Path As String
+        Dim istep As Integer
+        Dim Mat As Material = New Material("steel")
+        Dim element As List(Of Integer) = New List(Of Integer)
+        Dim count As Integer
+        Dim count2 As Integer
+        Dim NoOfVertices As Integer
+        Dim NoOfElement As Integer
+        Path = "C:\Users\isenturk\Documents\irem.txt"
+
+        Dim triangles As List(Of Element) = New List(Of Element)
+        Dim vertices As List(Of Point3D)
+        For Each item As String In streamClass.FileRead(Path).ToList
+
+            If (istep = 1) And (count2 < NoOfVertices) Then
+                translate.SumVertices(vertices, item)
+                count2 = count2 + 1
+            ElseIf (istep = 1) And (count2 = NoOfVertices) Then
+                istep = 0
+                count2 = 0
+            End If
+            If (istep = 2) And (count < NoOfElement) Then
+                translate.SumElement(Mat, vertices, item)
+                count = count + 1
+            ElseIf (istep = 2) And (count = NoOfElement) Then
+                istep = 0
+                count = 0
+            End If
+
+
+            If item.Contains("elements:") Then
+                Dim ForSplit = Split(item, " ")
+                Integer.TryParse(ForSplit(1), NoOfElement)
+                istep = 2
+            ElseIf item.Contains("vertices:") Then
+                vertices = New List(Of Point3D)
+                Dim ForSplit = Split(item, " ")
+                Integer.TryParse(ForSplit(1), NoOfVertices)
+                istep = 1
+            End If
+        Next
+        sim1.Invalidate()
+    End Sub
+
+End Class
+
